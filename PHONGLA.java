822d65d5ea50639fa5c145ea332da28b0e81bd64
LeoSunPhong <laquyphong1685@gmail.com>
2025-05-16 10:24:05 +0700

    Comment

--- PHONGLA.java
+++ PHONGLA.java
@@ -8,12 +8,12 @@ package pjtest;
  *
  * @author Admin
  */
-public class HINHTRON extends javax.swing.JFrame {
+public class PHONGLA extends javax.swing.JFrame {
 
     /**
      * Creates new form ChuVI
      */
-    public HINHTRON() {
+    public PHONGLA() {
         initComponents();
     }
 
@@ -82,8 +82,8 @@ public class HINHTRON extends javax.swing.JFrame {
     private void btnDTActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDTActionPerformed
         // TODO add your handling code here:
         int a = Integer.parseInt(txtA.getText());
-        int b = Integer.parseInt(txtB.getText());
-        int dt = a*b;//// KIẾM CÔNG THỨC DIỆN TÍCH HÌNH TRÒN THAY VÀO ĐÂY
+         
+        int dt = (int) (3.14 * (a*a));
         lblShow.setText("DIEN TICH: " + dt);
     }//GEN-LAST:event_btnDTActionPerformed
 
@@ -104,21 +104,23 @@ public class HINHTRON extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HINHTRON.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PHONGLA.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HINHTRON.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PHONGLA.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HINHTRON.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PHONGLA.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HINHTRON.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PHONGLA.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new HINHTRON().setVisible(true);
+                new PHONGLA().setVisible(true);
             }
         });
     }
